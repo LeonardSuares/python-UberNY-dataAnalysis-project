017e777e923b0aa4d34d01d0395ef5012b547ecd
Leonard <Leonard.suares@gmail.com>
2025-12-06 12:44:16 -0500

    Visualize data for monthly and weekday ride counts

--- Uber-Ny-weekday-analysis.py
+++ Uber-Ny-weekday-analysis.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
-pd.set_option('display.max_columns', None)
+
 import  warnings
 from warnings import filterwarnings
 filterwarnings("ignore")
@@ -27,45 +27,18 @@ dup1 = uber_15.duplicated().sum()
 #TO drop the number of duplicate values in the CSV
 dupdrop = uber_15.drop_duplicates()
 
-# print(uber_15.shape)
-# print(dupdrop.shape)
+print(uber_15.shape)
+print(dupdrop.shape)
 # print(dupdrop)
 
 #TO find the number of null values in the CSV
 isnullitems = dupdrop.isnull().sum()
-# print(isnullitems)
+print(isnullitems)
 
 #TO find the datatypes and update the datatypes of columns
 csvdata = dupdrop.dtypes
-# print(csvdata)
+print(csvdata)
 
 # print(dupdrop.head(1))
 dupdrop["Pickup_date"] = pd.to_datetime(dupdrop["Pickup_date"])
-# print(dupdrop.dtypes)
-
-dupdrop['month'] = dupdrop['Pickup_date'].dt.month_name()
-# print(dupdrop['month'])
-
-#To Plot a graph = Bar chart
-# dupplot = dupdrop['month'].value_counts().plot(kind = 'bar')
-# # dupplot.plot(dupplot)
-# plt.ylabel("# of Rides")
-#
-# # Optional: Add x-axis label and a main title
-# plt.xlabel("Month")
-# plt.title("Uber NY Analysis")
-# plt.show()
-dupdrop['weekday'] = dupdrop['Pickup_date'].dt.day_name()
-dupdrop['day'] = dupdrop['Pickup_date'].dt.day
-dupdrop['hour'] = dupdrop['Pickup_date'].dt.hour
-dupdrop['minute'] = dupdrop['Pickup_date'].dt.minute
-# print(dupdrop.head(5))
-
-#To plot the weekly ride analysis
-pivot = pd.crosstab(index = dupdrop['month'], columns= dupdrop['weekday'])
-pivotplot = pivot.plot(kind = 'bar', figsize = (8,6))
-plt.ylabel("# of Rides")
-plt.xlabel("Month")
-plt.title("Uber NY weekday Analysis")
-plt.show()
-#To confirm the file is up-to-date
+print(dupdrop.dtypes)
